726dc1a23a9f1da42b6d390f5c9c7722b14848d7
Oliver Wehrens <oliver.wehrens@smava.de>
2022-12-28 15:16:37 +0100

    Corrected file.

--- run_python_whisper.sh
+++ run_python_whisper.sh
@@ -2,7 +2,7 @@
 
 
 if [ ! -f audio.mp3 ]; then
-    wget https://adn.podigee.com/adswizz/media/podcast_1652_was_jetzt_episode_847930_update_eine_gefahrliche_mission.mp3 -O audio.mp3 
+    wget https://adn.podigee.com/adswizz/media/podcast_1652_was_jetzt_episode_818856_update_ein_notstand_aber_keine_zweite_pandemie.mp3 -O audio.mp3
     ffmpeg -y -i audio.mp3 -ar 16000 -ac 1 -c:a pcm_s16le audio.wav 
 fi
 
